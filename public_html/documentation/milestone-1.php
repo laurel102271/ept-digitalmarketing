0560684149ca73b9c6d791394d95bf7157c0b0d7
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-18 17:48:35 -0600

    PWP Website

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Digital marketing</title>
+	</head>
+	<body>
+		<H1>Purpose</H1><p>I want my website to attract business owners in the El Paso Texas area in need of a business website and to help the client grow there customer base by building them a website and ranking them on the first page of google with a marketing campaign that will use keywords and seo. </p>
+		<h1>Audience</h1><p>My target audience will be male and female demographics between the age of 18-65 years old who are in need of a business website.</p>
+		<h1>Goals</h1>
+		<ul>
+			<li>To acquire 4 new business per month for the first 6 months. </li>
+			<li>To make physical contact with 5 business owners per day and talk about there business goals for growing there business. </li>
+			<li>To make 20 phone calls per day to small business owners. </li>
+		</ul>
+		<h1>Persona</h1>
+		<p>The demographics that I will target are male and female clients between the ages of 18-65 years of age that are self employed and are accustomed to  diffrent technology platform as example apple or android and are looking for help growing there business through a business website. Most small business owners that I will target will have been operating a business without a website and it  will be my marketing objective to convince them on their opinion that they do need a website when there behavior and actions have shown that they dont strongly believe in building a website. The way I plan on building the clients website will be to have a understanding of the clients business and implement that into a website with a form and linking the website with a google adwords and also providing seo for organic ranking in google search engines. </p>
+	</body>
+</html>
